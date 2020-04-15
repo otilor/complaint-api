397a7087d2dd5b5c9c65d4240d046f07ab94b8c7
GabrielFemi <gabrielfemi799@gmail.com>
2020-04-15 09:40:12 +0100

    finished api

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+
+$response_type = "xml";
+/**
+ * Connect to the database
+ * 
+ * @var  $conn
+ * @return $conn
+ */
+
+ function conn()
+ {
+    $conn = mysqli_connect("localhost", "gabriel", "Portedjester_20", "complaint_api");
+    if (!$conn)
+    {
+        return null;
+    }
+    return $conn;
+ }
+/**
+ * data function
+ */
+function data($conn)
+{
+    $conn = conn($conn);
+    $fetch_sql = "SELECT * FROM complaints";
+    $fetch_query = mysqli_query($conn, $fetch_sql);
+    $fetch_result = mysqli_fetch_all($fetch_query, MYSQLI_ASSOC);
+    $accepted_complaints = [];
+    $total_complaints = count($fetch_result);
+    $messages = [];
+
+    foreach ($fetch_result as $result)
+    {
+        if ($result["accepted?"] == 1)
+        {
+            array_push($accepted_complaints, $result);
+        }
+    }
+
+    foreach ($fetch_result as $message)
+    {
+        array_push ($messages, $message["message"]);
+    }
+    
+    
+    if (!is_null($conn))
+    {
+        
+        
+        $data = [
+            "messages" => $messages,              
+            "status" => 200,
+            "totalComplaints" => count($fetch_result),
+            "acceptedComplaints" => count($accepted_complaints),
+        ];
+    
+        return $data;
+    }
+    else
+    {
+        return "Failed";
+    }
+    
+    
+}
+
+function array2xml($array, $xml=false)
+{
+    if ($xml === false)
+    {
+        $xml = new SimpleXMLElement('<result/>');
+    }
+    foreach ($array as $key => $value)
+    {
+        if (is_array($value))
+        {
+            array2xml($value, $xml->addChild($key));
+        }
+        else{
+            $xml->addChild($key, $value);
+        }
+        
+    }
+    return $xml->asXML();
+}
+
+
+
+if(!$response_type == "xml")
+{
+    header("Content-Type: application/json");
+    echo json_encode(data(conn()));
+}
+header('Content-Type: text/xml');
+$data = data(conn());
+$xml = array2xml($data, false);
+print_r($xml);
